764d453d9d1eed293681416481f73143afb970f4
cc <zhanghc_qm@163.com>
2020-06-08 17:50:59 +0800

    socore

--- test_sample/while循环/socre.py
+++ test_sample/while循环/socre.py
@@ -1,15 +1,15 @@
 i=0
 while i<3:
- score = float(input('please enter a num:'))
- if score>=0 and score <=100:
-    if score > 80:
-        print('您的分数是：%s'%score,'优秀')
-    elif score > 60:
-        print('您的分数是：%s'%score,'及格')
-    elif score > 0:
-        print('您的分数是：%s'%score,'不及格')
- else:
-    print('您输入的数值不合法')
- i=i+1
+    score = float(input('please enter a num:'))
+    if 0 <= score <= 100:
+        if score > 80:
+            print('您的分数是：%s'% score,'优秀')
+        elif score > 60:
+            print('您的分数是：%s'% score,'及格')
+        elif score >= 0:
+            print('您的分数是：%s'% score,'不及格')
+    else:
+        print('请输入0-100的数字！')
+    i=i+1
 if i==3:
-    print('输入次数过多，请稍后再试！')
+    print('您的输入次数已达限制，请稍后输入！')
